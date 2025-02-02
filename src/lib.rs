0fc7b70fe3fd5926de407ced87786f7f491d51ef
Titouan Real <titouan.real@gmail.com>
2025-02-02 11:35:52 +0100

    Add lsp

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,18 @@ impl zed::Extension for VCardExtension {
     fn new() -> Self {
         Self {}
     }
+
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        _worktree: &zed::Worktree,
+    ) -> zed::Result<zed::Command> {
+        Ok(zed::Command {
+            command: "./vcard-lsp".to_owned(),
+            args: Vec::new(),
+            env: Vec::new(),
+        })
+    }
 }
 
 zed::register_extension!(VCardExtension);
